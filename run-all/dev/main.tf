c92ed0a654f903b1994d12396c3e292d53791799
Alon Kazimirsky <17432424+Wassap124@users.noreply.github.com>
2022-07-03 19:05:52 +0300

    remove db and subnet

--- run-all/dev/main.tf
+++ run-all/dev/main.tf
@@ -17,25 +17,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_db_subnet_group" "db_subnets" {
-  name       = "main"
-  subnet_ids = aws_subnet.tf-aws-sn[*].id
-
-  tags = {
-    Name = "My DB subnet group"
-  }
-}
-
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  db_subnet_group_name    = aws_db_subnet_group.db_subnets.name
-
-}
