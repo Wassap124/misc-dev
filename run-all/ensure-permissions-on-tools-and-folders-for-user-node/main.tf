34108b00a6be088a7624e0db982b649473d4169a
Alon Kazimirsky <17432424+Wassap124@users.noreply.github.com>
2022-07-03 17:19:24 +0300

    providers

--- run-all/ensure-permissions-on-tools-and-folders-for-user-node/main.tf
+++ run-all/ensure-permissions-on-tools-and-folders-for-user-node/main.tf
@@ -12,3 +12,29 @@ resource "aws_db_instance" "example2" {
 	parameter_group_name = "default.mysql5.7"
 	skip_final_snapshot  = true
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "example" {
+	allocated_storage    = 10
+	engine               = "mysql"
+	engine_version       = "5.7"
+	instance_class       = "db.t3.micro"
+	name                 = "mydb"
+	username             = "foo"
+	password             = "foobarbaz"
+	parameter_group_name = "default.mysql5.7"
+	skip_final_snapshot  = true
+}
