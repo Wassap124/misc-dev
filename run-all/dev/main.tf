99f2279718d4ea4b721501b73e52f2983e9edb8e
Alon Kazimirsky <17432424+Wassap124@users.noreply.github.com>
2022-07-03 19:25:18 +0300

    null resource

--- run-all/dev/main.tf
+++ run-all/dev/main.tf
@@ -1,15 +1,5 @@
 /* empty file - Terragrunt will generate all resources */
 
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+resource "null_resource" "null" {
+    
 }
