9bb89d32c6054b23f76d90809f43e51ed46c1d0f
Alon Kazimirsky <17432424+Wassap124@users.noreply.github.com>
2022-07-04 15:31:22 +0300

    remove a resource

--- run-all/ensure-permissions-on-tools-and-folders-for-user-node/main.tf
+++ run-all/ensure-permissions-on-tools-and-folders-for-user-node/main.tf
@@ -1,14 +1,3 @@
+resource "null_resource" "null5" {
 
-resource "aws_s3_bucket" "alon_test_bucket2" {
-  bucket = "alon-test-bucket-2"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_acl" "example11" {
-  bucket = aws_s3_bucket.alon_test_bucket2.id
-  acl    = "private"
 }
