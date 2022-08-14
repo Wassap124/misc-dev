4418e1dda06089e662dc6aa6206150ca16388b38
Alon Kazimirsky <17432424+Wassap124@users.noreply.github.com>
2022-08-14 10:43:33 +0300

    Update main.tf

--- module-reg-test/main.tf
+++ module-reg-test/main.tf
@@ -1,4 +1,4 @@
 module "my-module" {
-  source = "api-pr8870.dev.env0.com/7e0f9e56-b199-44da-ba70-08987691ba81/test/testprovider"
+  source = "api-pr8870.dev.env0.com/7e0f9e56-b199-44da-ba70-08987691ba81/test-2/alon"
   version = "0.0.2"
 }
