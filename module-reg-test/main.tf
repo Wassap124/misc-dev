a7724f489f0f50d8d2116a912c1e25da50b04487
Alon Kazimirsky <17432424+Wassap124@users.noreply.github.com>
2022-08-17 11:26:07 +0300

    Update main.tf

--- module-reg-test/main.tf
+++ module-reg-test/main.tf
@@ -1,4 +1,4 @@
 module "my-module" {
-  source = "api-pr8870.dev.env0.com/7e0f9e56-b199-44da-ba70-08987691ba81/test/provider"
+  source = "api-pr8962.dev.env0.com/a81e6ea4-9c48-4d22-81af-2498a582d021/test/provider"
   version = "0.0.2"
 }
