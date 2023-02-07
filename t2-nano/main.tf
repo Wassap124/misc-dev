1e7f64f4c15e8b6efec544adcd10ad9856a5009c
Alon Kazimirsky <env0-bot@env0.com>
2023-02-07 12:42:12 +0200

    ec2 instance

---
+++ t2-nano/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# t2.Nano
+resource "aws_instance" "foo" {
+  instance_type = "t2.nano"
+}
