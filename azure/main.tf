8cc94d3eefb355644922ae8ebd94bc371175295e
Alon Kazimirsky <env0-bot@env0.com>
2024-01-15 18:50:30 +0200

    update provider definition

--- azure/main.tf
+++ azure/main.tf
@@ -1,6 +1,9 @@
-provider "azurerm" {
-  version = "3.28.0"
-  features {
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
   }
 }
 
