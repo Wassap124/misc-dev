33f74987b0d6cce8e3eb6d82236a79e08d175db6
Alon Kazimirsky <17432424+Wassap124@users.noreply.github.com>
2024-01-16 17:52:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "null_resource" "resource1" {
+resource "null_resource" "resource2" {
 	count = 4
 }
 
-resource "null_resource" "resource2" {
+resource "null_resource" "resource3" {
 }
 
 module "local_module" {
