176c543c96f62d730a78f8506978fceaaf3ae89d
Alon Kazimirsky <env0-bot@env0.com>
2024-02-05 14:08:46 +0200

    use ec2 instance

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
 resource "null_resource" "resource2" {
 	count = 4
 }
@@ -7,4 +17,29 @@ resource "null_resource" "resource3" {
 
 module "local_module" {
   source = "./module"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
