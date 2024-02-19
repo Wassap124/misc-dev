3d8a4195955fd51283754a2b889345dd7f6161c8
Alon Kazimirsky <env0-bot@env0.com>
2024-02-19 17:05:15 +0200

    move terragrunt to its own folder

--- terragrunt/main.tf
+++ terragrunt/main.tf
@@ -1,19 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
 }
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 
 resource "null_resource" "resource2" {
-	count = 4
+  count = 4
 }
 
 resource "null_resource" "resource3" {
