07e9111dcecd657e96a008022d709001588f3765
Alon Kazimirsky <env0-bot@env0.com>
2024-03-04 19:20:17 +0200

    hack

---
+++ hackathon/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "null_resource" "resource2" {
+  count = 1000
+}
