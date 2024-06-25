aaefdce4591a4cf713e5739a3c90a84fbe91aa8d
Alon Kazimirsky <env0-bot@env0.com>
2024-06-25 17:23:32 +0300

    add sleep template

---
+++ sleep-null/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "null_resource" "example" {
+}
+
+output "null_resource_id" {
+  value = null_resource.example.id
+}
