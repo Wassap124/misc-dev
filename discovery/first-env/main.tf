12c8b5a83c0c6b0c5c56ece1c8d692489acd9497
Alon Kazimirsky <env0-bot@env0.com>
2024-08-21 14:17:11 +0300

    add discovery folder and some tf code

---
+++ discovery/first-env/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "null_resourasdace" "example" {
+  count = 3
+}
+
+output "null_resource_id" {
+  value = null_resource.example[0].id
+}
+
+variable "sub_tf_variable" {
+  type        = string   # The data type (string, number, bool, list, map, etc.)
+  default     = "default_value"  # Optional, provides a default value
+}
+
+
+output "output_for_pismo" {
+  value = var.sub_tf_variable
+}
