3ed3d3c6215ee9b76f93e57b457059a33c54e49a
Wassap124 <alonkazimirsky@gmail.com>
2024-12-23 17:02:25 +0200

    add oracle cloud config

---
+++ oracle-cloud/main.tf
@@ -0,0 +1,15 @@
+provider "oci" {
+  # Assumes configuration via environment variables
+}
+
+variable "compartment_ocid" {
+  description = "Compartment OCID"
+  type        = string
+  default     = "ocid1.tenancy.oc1..aaaaaaaa2olq7iivwq3kbwnqfhiefpyyllxgkmnhbm7wbgmy73fprk5jozaq"
+}
+
+resource "oci_objectstorage_bucket" "simple_bucket" {
+  name           = "my-simple-bucket"
+  compartment_id = var.compartment_ocid
+  storage_tier   = "Standard" # Options: "Standard" or "Archive"
+}
